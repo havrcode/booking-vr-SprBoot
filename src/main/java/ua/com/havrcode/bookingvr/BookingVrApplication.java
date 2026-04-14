2f06b86d27de381c52521f938054d372a986397d
havrcode <havrcode@ukr.net>
2026-04-15 00:02:39 +0300

    Initial commit

---
+++ src/main/java/ua/com/havrcode/bookingvr/BookingVrApplication.java
@@ -0,0 +1,13 @@
+package ua.com.havrcode.bookingvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingVrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingVrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/havrcode/bookingvr/BookingVrApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.havrcode.bookingvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingVrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
