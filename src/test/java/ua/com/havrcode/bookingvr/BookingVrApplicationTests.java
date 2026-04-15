f80f892489a3e466313893d0b736bc5788faf42c
havrcode <havrcode@ukr.net>
2026-04-15 19:41:58 +0300

    add structure dir in project

--- src/test/java/ua/com/havrcode/bookingvr/BookingVrApplicationTests.java
+++ src/test/java/ua/com/havrcode/bookingvr/BookingVrApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookingVrApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
